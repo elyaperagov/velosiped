6811ef61ccc7fb2e88190f294088ae3cc361dd2e
Елья <piragof@gmail.com>
2020-04-23 02:36:38 +0400

    добавляет планшетное разрешение+открытие и закрытие меню

--- build/js/main.js
+++ build/js/main.js
@@ -1,8 +1,29 @@
 'use strict';
 ymaps.ready(init);
-  function init(){
-    var myMap = new ymaps.Map("map", {
-       center: [59.938635, 30.323118],
-       zoom: 16
-    });
- }
+function init() {
+  var myMap = new ymaps.Map('map', {
+    center: [59.938635, 30.323118],
+    zoom: 16
+  });
+}
+
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var header = document.querySelector('.header');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    header.classList.remove('page-header--absolute');
+    header.classList.add('page-header--relative');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    header.classList.add('page-header--absolute')
+    header.classList.remove('page-header--relative');
+  }
+});
