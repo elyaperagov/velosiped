311e063d061f763098718dc5cadfe6dbd668a100
Елья <piragof@gmail.com>
2020-04-24 04:33:39 +0400

    добавляет мобильное разрешение

--- build/js/main.js
+++ build/js/main.js
@@ -7,23 +7,40 @@ function init() {
   });
 }
 
-
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 var header = document.querySelector('.header');
+var input = document.querySelectorAll('input');
+var button = document.querySelector('.button--submit');
+var main = document.querySelector('main');
 
-navMain.classList.remove('main-nav--nojs');
+if (navMain) {
+  navMain.classList.remove('main-nav--nojs');
+  navMain.classList.add('main-nav--closed');
+}
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    header.classList.remove('page-header--absolute');
-    header.classList.add('page-header--relative');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    header.classList.add('page-header--absolute')
-    header.classList.remove('page-header--relative');
+var changeFormState = function (object, newState) {
+  for (var i = 0; i < object.length; i++) {
+    object[i].disabled = newState;
   }
-});
+};
+
+if (navToggle) {
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+      changeFormState(input, true);
+      button.setAttribute("disabled", "");
+      document.body.style.position = 'hidden';
+      main.style.opacity = '0.7';
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+      document.body.style.overflow = 'visible';
+      main.style.opacity = '1';
+      changeFormState(input, false);
+      button.removeAttribute("disabled", "");
+    }
+  });
+}
