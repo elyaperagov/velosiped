54be1c4b5fa6e0deab82a6644e3a94001161e59b
Елья <piragof@gmail.com>
2020-04-28 18:41:33 +0400

    отменяет все какие только вообще существуют в принципе скроллы при открытом меню

--- source/js/main.js
+++ source/js/main.js
@@ -11,7 +11,8 @@
   var typesLink = document.querySelector('.main-nav__item--types a');
   var videoLink = document.querySelector('.main-nav__item--video a');
   var locationLink = document.querySelector('.main-nav__item--location a');
-
+  var content = document.querySelector('.content');
+  var general = document.querySelector('.general');
 
   function fname(link) {
     $(link).on('click', function () {
@@ -50,20 +51,66 @@
     });
   };
 
+  var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+  var preventDefault = function (e) {
+    e = e || window.event;
+    if (e.preventDefault) {
+      e.preventDefault();
+      e.returnValue = false;
+    }
+  };
+
+  var preventDefaultForScrollKeys = function (e) {
+    if (keys[e.keyCode]) {
+      preventDefault(e);
+      return false;
+    }
+    return true;
+  };
+
+  var disableScroll = function () {
+    if (window.addEventListener) {
+      window.addEventListener('DOMMouseScroll', preventDefault, false);
+      document.addEventListener('wheel', preventDefault, {passive: false}); // Disable scrolling in Chrome
+      window.onwheel = preventDefault; // modern standard
+      window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+      window.ontouchmove = preventDefault; // mobile
+      document.onkeydown = preventDefaultForScrollKeys;
+    }
+  };
+
+  var enableScroll = function () {
+    if (window.removeEventListener) {
+      window.removeEventListener('DOMMouseScroll', preventDefault, false);
+      document.removeEventListener('wheel', preventDefault, {passive: false}); // Enable scrolling in Chrome
+      window.onmousewheel = document.onmousewheel = null;
+      window.onwheel = null;
+      window.ontouchmove = null;
+      document.onkeydown = null;
+    }
+  };
+
 
   var toggle = function () {
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
+      content.classList.add('content--faded');
+      general.classList.add('general--faded');
       changeFormState(input, true);
       document.addEventListener('keydown', isEscEvent);
       button.setAttribute('disabled', '');
       offScroll();
+      disableScroll();
     } else {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
+      content.classList.remove('content--faded');
+      general.classList.remove('general--faded');
       document.removeEventListener('keydown', isEscEvent);
       $(window).unbind('scroll');
+      enableScroll();
       changeFormState(input, false);
       button.removeAttribute('disabled', '');
     }
@@ -79,8 +126,6 @@
     navItems[i].addEventListener('click', function () {
       if (document.documentElement.clientWidth < 1024) {
         toggle();
-      } else {
-        return;
       }
     });
   }
